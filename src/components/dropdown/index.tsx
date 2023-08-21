d183aa4b77ca9fb5f754c1a06f7ed804d6aae445
Muhammed-baban <93913650+Muhammed-baban@users.noreply.github.com>
2023-08-21 15:07:15 +0300

    dropdonw yapıldı.

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,12 +1,44 @@
-import './index.css';
+import React, { useState } from "react";
+import "./index.css";
+
+interface Option{
+    label: string;
+    url:string;
+    value: string;
+}
+
 
 const Dropdown = () => {
-    return <div className="dropdown-container">
-        <span className="styled-dropdown"></span>
-        <span className="styled-dropdown"></span>
-        <span className="styled-dropdown"></span>
-        </div>;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const options: Option[]=[
+        {label:"Seçenek 1",url:"#",value:"option1"},
+        {label:"Seçenek 2",url:"#",value:"option2"},
+        {label:"Seçenek 3",url:"#",value:"option3"}
+    ]
+    
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="dropdown">
+            <button className="dropbtn" onClick={toggleDropdown}>
+                <span className="styled-dropdown"></span>
+                <span className="styled-dropdown"></span>
+                <span className="styled-dropdown"></span>
+            </button>
+            {isOpen && (
+                <div className="dropdown-content">
+                   {options.map(option => (
+                        <a key={option.value} href={option.url}>
+                            {option.label}
+                        </a>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Dropdown;
- 
--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,8 +1,10 @@
+import Dropdown from "../../components/dropdown";
 
 const Todo = () => {
   return (
     <div>
       <h1>Todo App</h1>
+      <Dropdown />
     </div>
   );
 };
