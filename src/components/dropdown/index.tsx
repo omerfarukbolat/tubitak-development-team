63a63aa330f325c251c9717ec86dafe814ce3c17
Muhammed-baban <93913650+Muhammed-baban@users.noreply.github.com>
2023-08-21 13:40:56 +0300

    dropdonw yapıldı.

---
+++ src/components/dropdown/index.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+const Dropdown = () => {
+    return <div className="dropdown-container">
+        <span className="styled-dropdown"></span>
+        <span className="styled-dropdown"></span>
+        <span className="styled-dropdown"></span>
+        </div>;
+};
+
+export default Dropdown;
+ 
