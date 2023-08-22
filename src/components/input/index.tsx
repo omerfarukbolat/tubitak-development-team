1d7b2d08c4e5c9faf52ad2b8d7bceac58317b1a6
omerfarukbolat <omer.bolat@dnext-technology.com>
2023-08-22 10:42:22 +0300

    input form width change fixed

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -24,10 +24,10 @@ const Input: React.FC<InputProps> = ({
   };
 
   return (
-    <div className="styled-input">
+    <div className={`styled-input ${maxWidth ? 'maxWidth' : ''}`}>
       <label className="styled-input-label">{label}</label>
       <input
-        className={`styled-input-form ${maxWidth ? 'maxWidth' : ''}`}
+        className="styled-input-form"
         placeholder={placeholder}
         value={value}
         name={name}
