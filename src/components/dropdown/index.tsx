f7004428499ff10e6bd2ba72611b8cac25e6976d
omerfarukbolat <omer.bolat@dnext-technology.com>
2023-08-23 12:28:30 +0300

    dropdown is fixed

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,44 +1,56 @@
-import React, { useState } from "react";
-import "./index.css";
+import { useState, useRef } from 'react';
+import './dropdown.css';
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 interface DropdownData {
-    name: string;
-    click: () => void;
-
+  name: string;
+  click: () => void;
 }
 interface DropdownProps {
-    data: DropdownData[];
-    isReverse:boolean;
-    
+  data: DropdownData[];
+  isReverse?: boolean;
 }
 
+const Dropdown: React.FC<DropdownProps> = ({ data, isReverse = false }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  useOnClickOutside(ref, () => setIsOpen(false));
 
-const Dropdown: React.FC<DropdownProps> = ({data}) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const listItemClicking = (click: () => void) => {
+    click();
+    setIsOpen(false);
+  };
 
-
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
-
-    return (
-        <div className="styled-dropdown">
-            <button className="styled-dropdown-button" onClick={toggleDropdown}>
-                <span className="styled-dropdown-dot"></span>
-                <span className="styled-dropdown-dot"></span>
-                <span className="styled-dropdown-dot"></span>
-            </button>
-            {isOpen && (
-                <div className="styled-dropdown-content">
-                    {data.map(data => (
-                        <a key={data.name} onClick={data.click}>
-                            {data.name}
-                        </a>
-                    ))}
-                </div>
-            )}
+  return (
+    <div ref={ref} className="styled-dropdown">
+      <button
+        className="styled-dropdown-button"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="styled-dropdown-dot"></span>
+        <span className="styled-dropdown-dot"></span>
+        <span className="styled-dropdown-dot"></span>
+      </button>
+      {isOpen && (
+        <div
+          className={`styled-dropdown-menu ${isReverse ? 'top' : 'bottom'}`}
+          style={{ top: isReverse ? -(data.length * 40 + 20) : 30 }}
+        >
+          <ul className="styled-dropdown-menu-list">
+            {data.map((item) => (
+              <li
+                className="styled-dropdown-menu-list-item"
+                key={item.name}
+                onClick={() => listItemClicking(item.click)}
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Dropdown;
