35633f131f040ebea3411cf1c1d673095ef543c9
omerfarukbolat <omer.bolat@dnext-technology.com>
2023-08-24 15:00:19 +0300

    container interface bug fixed

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import './container.css';
 
 interface ContainerProps {
-  children: JSX.Element;
+  children: JSX.Element | JSX.Element[];
 }
 
 const Container: React.FC<ContainerProps> = ({ children }) => (
-  <div className="styled-container">{children}</div>
+  <div className="styled-container">
+    <React.Fragment>{children}</React.Fragment>
+  </div>
 );
 
 export default Container;
