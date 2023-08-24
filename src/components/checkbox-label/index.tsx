170c3213b2c16fe284570dd5bd9228d8284ec8fc
Muhammed-baban <93913650+Muhammed-baban@users.noreply.github.com>
2023-08-24 10:17:57 +0300

    active setactive kaldırıldı labela margin-left eklendi

--- src/components/checkbox-label/index.tsx
+++ src/components/checkbox-label/index.tsx
@@ -3,25 +3,27 @@ import "./index.css";
 
 interface checkboxlabelProps {
   label: string;
-  active: boolean;
-  setActive: () => void;
+
 }
 
-const CheckboxLabel: React.FC<checkboxlabelProps> = ({ label, active, setActive }) => {
-  const [isChecked, setIsChecked] = useState(active);
+const CheckboxLabel: React.FC<checkboxlabelProps> = ({ label }) => {
+  const [isChecked, setIsChecked] = useState(false);
 
   const handleCheckboxChange = () => {
-    setActive();
     setIsChecked(!isChecked);
   };
 
   return (
     <label className={`styled-checkbox-label ${isChecked ? "checked" : ""}`}>
-      <input 
-      type="checkbox" 
-      checked={isChecked} 
-      onChange={handleCheckboxChange} />
-      {label}
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheckboxChange} />
+
+      <span className="styled-checkbox-label-text">
+        {label}
+      </span>
+
     </label>
   );
 };
