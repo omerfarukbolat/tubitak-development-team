0edd51a90cc6476ddafbf1c986b00fad85caec96
omerfarukbolat <omer.bolat@dnext-technology.com>
2023-08-29 16:22:27 +0300

    component button new props size add fixed

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -5,6 +5,7 @@ interface ButtonProps {
   colour?: 'black' | 'white'; // We defined 2 colours for the Font Colour... If its not necessary..
   backgroundColour?: 'blue' | 'green' | 'red' | 'yellow'; // We defined 4 colours for the BackgroundColour... If its not necessary..
   onClick: () => void;
+  size: 'small' | 'medium' | 'large';
 }
 
 const Button = ({
@@ -12,9 +13,25 @@ const Button = ({
   colour = 'white',
   backgroundColour = 'blue',
   onClick,
+  size = 'medium',
 }: ButtonProps) => {
+  const sizeSelection = (size: string) => {
+    switch (size) {
+      case 'small':
+        return 5;
+      case 'medium':
+        return 10;
+      case 'large':
+        return 15;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <button
+      style={{ padding: sizeSelection(size) }}
       className={`styled-button colour-${colour} backgroundColour-${backgroundColour}`}
       onClick={onClick}
     >
