3e4f187ba586fc6a16508246eac929bbbf52e551
omerfarukbolat <omer.bolat@dnext-technology.com>
2023-09-04 13:12:24 +0300

    button back fixed

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -5,7 +5,6 @@ interface ButtonProps {
   colour?: 'black' | 'white'; // We defined 2 colours for the Font Colour... If its not necessary..
   backgroundColour?: 'blue' | 'green' | 'red' | 'yellow'; // We defined 4 colours for the BackgroundColour... If its not necessary..
   onClick: () => void;
-  size: 'small' | 'medium' | 'large';
 }
 
 const Button = ({
@@ -13,31 +12,13 @@ const Button = ({
   colour = 'white',
   backgroundColour = 'blue',
   onClick,
-  size = 'medium',
-}: ButtonProps) => {
-  const sizeSelection = (size: string) => {
-    switch (size) {
-      case 'small':
-        return 5;
-      case 'medium':
-        return 10;
-      case 'large':
-        return 15;
-
-      default:
-        break;
-    }
-  };
-
-  return (
-    <button
-      style={{ padding: sizeSelection(size) }}
-      className={`styled-button colour-${colour} backgroundColour-${backgroundColour}`}
-      onClick={onClick}
-    >
-      {label}
-    </button>
-  );
-};
+}: ButtonProps) => (
+  <button
+    className={`styled-button colour-${colour} backgroundColour-${backgroundColour}`}
+    onClick={onClick}
+  >
+    {label}
+  </button>
+);
 
 export default Button;
