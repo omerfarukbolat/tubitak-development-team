b931d032fe240fff4fee008388fcb79ce1f80ba9
Ulaş Baran Tomak <ulasbaran.tomak@std.yeditepe.edu.tr>
2023-09-08 12:13:49 +0300

    updated components/button-props with maxWidth

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,6 +2,7 @@ import './button.css';
 
 interface ButtonProps {
   label: string;
+  maxWidth?: boolean;
   colour?: 'black' | 'white' | 'blue'; // We defined 2 colours for the Font Colour... If its not necessary..
   backgroundColour?: 'blue' | 'green' | 'red' | 'yellow' | 'white' | 'gray'; // We defined 4 colours for the BackgroundColour... If its not necessary..
   onClick: () => void;
@@ -9,12 +10,13 @@ interface ButtonProps {
 
 const Button = ({
   label,
+  maxWidth = false,
   colour = 'black',
   backgroundColour = 'white',
   onClick,
 }: ButtonProps) => (
   <button
-    className={`styled-button colour-${colour} backgroundColour-${backgroundColour}`}
+    className={`styled-button ${maxWidth ? 'maxWidth' : ''} colour-${colour} backgroundColour-${backgroundColour}`}
     onClick={onClick}
   >
     {label}
