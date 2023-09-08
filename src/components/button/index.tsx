afcb8a9d1b0c86f14315339000d58d3adf583025
Ulaş Baran Tomak <ulasbaran.tomak@std.yeditepe.edu.tr>
2023-09-08 11:51:02 +0300

    updated and fixed components/button-props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,15 +2,15 @@ import './button.css';
 
 interface ButtonProps {
   label: string;
-  colour?: 'black' | 'white'; // We defined 2 colours for the Font Colour... If its not necessary..
-  backgroundColour?: 'blue' | 'green' | 'red' | 'yellow'; // We defined 4 colours for the BackgroundColour... If its not necessary..
+  colour?: 'black' | 'white' | 'blue'; // We defined 2 colours for the Font Colour... If its not necessary..
+  backgroundColour?: 'blue' | 'green' | 'red' | 'yellow' | 'white' | 'gray'; // We defined 4 colours for the BackgroundColour... If its not necessary..
   onClick: () => void;
 }
 
 const Button = ({
   label,
-  colour = 'white',
-  backgroundColour = 'blue',
+  colour = 'black',
+  backgroundColour = 'white',
   onClick,
 }: ButtonProps) => (
   <button
