2310d8e4c58ec1d994b73be4447d18623fc791ea
omerfarukbolat <omer.bolat@dnext-technology.com>
2023-09-15 17:16:08 +0300

    changes fixed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,9 @@
-import Button from '../../components/button';
-import { openModal } from '../../store/reducers/modalReducer';
 import Container from '../../components/container';
-import { useAppDispatch } from '../../hooks/useAppDispatch';
 
-const Home = () => {
-  const dispatch = useAppDispatch();
-
-  return (
-    <Container>
-      <h1>Home Page</h1>
-      <Button
-        label="aç modal yeni eklemek"
-        onClick={() =>
-          dispatch(
-            openModal({
-              component: 'home-new-create',
-              title: 'Yeni kayıt',
-            })
-          )
-        }
-      />
-    </Container>
-  );
-};
+const Home = () => (
+  <Container>
+    <></>
+  </Container>
+);
 
 export default Home;
